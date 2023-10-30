5174c30fc4d833e0796eac9924618117cb371a86
unaiLarra <unai.larranaga@opendeusto.es>
2023-10-30 21:28:45 +0100

    Adicion de funciones __str__()

--- appJukeBox/models.py
+++ appJukeBox/models.py
@@ -3,12 +3,21 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nombre
+
 class Banda(models.Model):
     nombre = models.CharField(max_length=100)
     pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
     descripcion = models.TextField()
     estilos = models.ManyToManyField("Estilo", null=True)
+    
+    def __str__(self):
+        return self.nombre, self.pais.nombre, self.descripcion
 
 class Estilo(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre, self.descripcion
