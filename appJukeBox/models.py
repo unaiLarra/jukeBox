7f8c4cf3717daaca1839e2f32408dc7745dcf19c
unaiLarra <unai.larranaga@opendeusto.es>
2023-10-30 17:36:29 +0100

    Initial commit

---
+++ appJukeBox/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Banda(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
+    descripcion = models.TextField()
+    estilos = models.ManyToManyField("Estilo", null=True)
+
+class Estilo(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    bandas = models.ManyToManyField(Banda, null=True)
