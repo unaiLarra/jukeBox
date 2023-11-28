6e5e942bc474682d076c7b28454007c9de6c6cd4
unaiLarra <unai.larranaga@opendeusto.es>
2023-11-28 13:38:08 +0100

    Cambios en estlos y css de index

--- appJukeBox/static/js/effects.js
+++ appJukeBox/static/js/effects.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
 
   var is_up = false;
-  $("div.contenido > h2.listado").dbclick(function () {
-    if (is_up == false) {
+  $("h2.listado").dbclick(function () {
       is_up = true;
       $("div.bandas").slideUp();
-    } else {
-      is_up = false;
-      $("div.bandas").slideDown();
-    }
   });
 
  });
