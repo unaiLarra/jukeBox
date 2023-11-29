314febf81e78be9ea257e8f35d43a3d6da45f401
unaiLarra <unai.larranaga@opendeusto.es>
2023-11-29 11:56:54 +0100

    CSS y html completado en su enorme  mayoría

--- appJukeBox/urls.py
+++ appJukeBox/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index_pais_banda, name='index'),
     path('estilos', views.index_estilos, name='estilos'),
-    path('estilos/<int:estilo_id>/', views.show_estilo, name='estilo'),
+    path('estilos/<int:estilo_id>', views.show_estilo, name='estilo'),
     path('bandas', views.index_bandas, name='bandas'),
     path('bandas/<int:banda_id>', views.show_banda, name='banda'),
     path('paises', views.index_paises, name="paises"),
